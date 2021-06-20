d700d6291fc9e2916912152cf4cb260e3af881c1
VezRoland <vezrolaa@gmail.com>
2021-06-20 15:47:20 +0200

    Update kod.js

--- kod.js
+++ kod.js
@@ -1,6 +1,8 @@
 window.addEventListener("load", meretezes);
 window.addEventListener("resize", meretezes);
 window.addEventListener("orientationchange", meretezes);
+var szel = window.innerWidth;
+var mag = window.innerHeight;
 var logo = document.getElementById("logo");
 var fnev = document.getElementById("fnev");
 var email = document.getElementById("email");
@@ -8,14 +10,27 @@ var jszo = document.getElementById("jszo");
 var gomb = document.getElementById("gomb");
 
 function meretezes() {
+    var meret = window.innerWidth + window.innerHeight;
     document.body.style.width = window.outerWidth + "px";
     document.body.style.height = window.innerHeight + "px";
     console.log(window.outerHeight)
-    meret = window.innerWidth + window.innerHeight;
-    console.log(meret)
-    logo.style.width = meret / 5.5 + "px";
-    fnev.style.width = meret / 5.5 + "px";
-    email.style.width = meret / 5.5 + "px";
-    jszo.style.width = meret / 5.5 + "px";
-    gomb.style.width = (meret / 5.5) + 22 + "px";  
+    if (szel != window.innerWidth) {
+        logo.style.width = meret / 5 + "px";
+        fnev.style.width = meret / 5 + "px";
+        email.style.width = meret / 5 + "px";
+        jszo.style.width = meret / 5 + "px";
+        gomb.style.width = (meret / 5) + 22 + "px";
+    } else if (window.innerWidth > 920) {
+        logo.style.width = window.innerWidth / 3.5 + "px";
+        fnev.style.width = window.innerWidth / 3.5 + "px";
+        email.style.width = window.innerWidth / 3.5 + "px";
+        jszo.style.width = window.innerWidth / 3.5 + "px";
+        gomb.style.width = (window.innerWidth / 3.5) + 22 + "px";
+    } else {
+        logo.style.width = window.innerWidth / 1.5 + "px";
+        fnev.style.width = window.innerWidth / 1.5 + "px";
+        email.style.width = window.innerWidth / 1.5 + "px";
+        jszo.style.width = window.innerWidth / 1.5 + "px";
+        gomb.style.width = (window.innerWidth / 1.5) + 22 + "px";
+    }
 }
