838a7a40001a6e1d26dd7ed3f7af7506f062e2ae
VezRoland <vezrolaa@gmail.com>
2021-06-20 17:18:20 +0200

    Update kod.js

--- kod.js
+++ kod.js
@@ -1,8 +1,8 @@
 window.addEventListener("load", meretezes);
 window.addEventListener("resize", meretezes);
 window.addEventListener("orientationchange", meretezes);
-var szel = window.innerWidth;
-var mag = window.innerHeight;
+var szel = window.screen.availWidth;
+var mag = window.screen.availHeight;
 var logo = document.getElementById("logo");
 var fnev = document.getElementById("fnev");
 var email = document.getElementById("email");
@@ -10,27 +10,27 @@ var jszo = document.getElementById("jszo");
 var gomb = document.getElementById("gomb");
 
 function meretezes() {
-    var meret = window.innerWidth + window.innerHeight;
-    document.body.style.width = window.innerWidth + "px";
-    document.body.style.height = window.innerHeight + "px";
-    console.log(window.outerHeight)
-    if (szel != window.innerWidth) {
+    var meret = window.screen.availWidth + window.screen.availHeight;
+    document.body.style.width = window.screen.availWidth + "px";
+    document.body.style.height = window.screen.availHeight + "px";
+    console.log(window.screen.availWidth, window.screen.availHeight)
+    if (szel != window.screen.availWidth) {
         logo.style.width = meret / 5 + "px";
         fnev.style.width = meret / 5 + "px";
         email.style.width = meret / 5 + "px";
         jszo.style.width = meret / 5 + "px";
         gomb.style.width = (meret / 5) + 22 + "px";
-    } else if (window.innerWidth > 920) {
-        logo.style.width = window.innerWidth / 3.5 + "px";
-        fnev.style.width = window.innerWidth / 3.5 + "px";
-        email.style.width = window.innerWidth / 3.5 + "px";
-        jszo.style.width = window.innerWidth / 3.5 + "px";
-        gomb.style.width = (window.innerWidth / 3.5) + 22 + "px";
+    } else if (window.screen.availWidth > 920) {
+        logo.style.width = window.screen.availWidth / 3.5 + "px";
+        fnev.style.width = window.screen.availWidth / 3.5 + "px";
+        email.style.width = window.screen.availWidth / 3.5 + "px";
+        jszo.style.width = window.screen.availWidth / 3.5 + "px";
+        gomb.style.width = (window.screen.availWidth / 3.5) + 22 + "px";
     } else {
-        logo.style.width = window.innerWidth / 1.5 + "px";
-        fnev.style.width = window.innerWidth / 1.5 + "px";
-        email.style.width = window.innerWidth / 1.5 + "px";
-        jszo.style.width = window.innerWidth / 1.5 + "px";
-        gomb.style.width = (window.innerWidth / 1.5) + 22 + "px";
+        logo.style.width = window.screen.availWidth / 1.5 + "px";
+        fnev.style.width = window.screen.availWidth / 1.5 + "px";
+        email.style.width = window.screen.availWidth / 1.5 + "px";
+        jszo.style.width = window.screen.availWidth / 1.5 + "px";
+        gomb.style.width = (window.screen.availWidth / 1.5) + 22 + "px";
     }
 }
