f4bb836d91f18d747fc355658beec1e43660acc3
VezRoland <vezrolaa@gmail.com>
2021-06-20 15:38:40 +0200

    Update kod.js

--- kod.js
+++ kod.js
@@ -1,6 +1,6 @@
-window.addEventListener("resize", meretezes);
 window.addEventListener("load", meretezes);
-var meret = window.innerWidth;
+window.addEventListener("resize", meretezes);
+window.addEventListener("orientationchange", meretezes);
 var logo = document.getElementById("logo");
 var fnev = document.getElementById("fnev");
 var email = document.getElementById("email");
@@ -8,18 +8,14 @@ var jszo = document.getElementById("jszo");
 var gomb = document.getElementById("gomb");
 
 function meretezes() {
-    meret = window.innerWidth;
-    if (meret > 960) {
-        logo.style.width = meret / 3.5 + "px";  
-        fnev.style.width = meret / 3.5 + "px";
-        email.style.width = meret / 3.5 + "px";
-        jszo.style.width = meret / 3.5 + "px";
-        gomb.style.width = (meret / 3.5) + 22 + "px";  
-    } else {
-        logo.style.width = meret / 1.5 + "px"; 
-        fnev.style.width = meret / 1.5 + "px";
-        email.style.width = meret / 1.5 + "px";
-        jszo.style.width = meret / 1.5 + "px";
-        gomb.style.width = (meret / 1.5) + 22 + "px";
-    }  
+    document.body.style.width = window.outerWidth + "px";
+    document.body.style.height = window.innerHeight + "px";
+    console.log(window.outerHeight)
+    meret = window.innerWidth + window.innerHeight;
+    console.log(meret)
+    logo.style.width = meret / 5.5 + "px";
+    fnev.style.width = meret / 5.5 + "px";
+    email.style.width = meret / 5.5 + "px";
+    jszo.style.width = meret / 5.5 + "px";
+    gomb.style.width = (meret / 5.5) + 22 + "px";  
 }
